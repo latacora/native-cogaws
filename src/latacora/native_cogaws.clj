d36758b2f852102a11cfd2aa22399f45d738d7a0
Sergey Alekhnovich <sergey@latacora.com>
2020-09-17 14:17:57 -0700

    no config; cannot find resource cognitect/aws/s3/service.edn
    error:
    Exception in thread "main" clojure.lang.ExceptionInfo: Cannot find resource
    cognitect/aws/s3/service.edn. {}
       at cognitect.aws.service$service_description.invokeStatic(service.clj:29)
       at cognitect.aws.client.api$client.invokeStatic(api.clj:71)
       at latacora.native_cogaws$_main.invokeStatic(native_cogaws.clj:5)
       at latacora.native_cogaws$_main.doInvoke(native_cogaws.clj:5)
       at clojure.lang.RestFn.invoke(RestFn.java:397)
       at clojure.lang.AFn.applyToHelper(AFn.java:152)
       at clojure.lang.RestFn.applyTo(RestFn.java:132)
       at latacora.native_cogaws.main(Unknown Source)

--- src/latacora/native_cogaws.clj
+++ src/latacora/native_cogaws.clj
@@ -1,7 +1,13 @@
 (ns latacora.native-cogaws
+  (:require [cognitect.aws.client.api :as aws])
   (:gen-class))
+
+(set! *warn-on-reflection* true)
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Hello, World! - I'm a native thing!")
+  (let [s3 (aws/client {:api :s3})]
+    (println "s3 client has been initialized")
+    ))

2ea7fb1add3db72a30a21c675c3b45480f29a455
Sergey Alekhnovich <sergey@latacora.com>
2020-09-17 18:01:48 -0700

    try explicitly requiring the cognitect.aws.http.cognitect namespace
    error:
    Exception in thread "main" java.security.NoSuchAlgorithmException: Error constructing implementation (algorithm: TLS, provider: SunJSSE, class: sun.security.ssl.SSLContextImpl$TLSContext)
            at java.security.Provider$Service.newInstance(Provider.java:1902)
            at sun.security.jca.GetInstance.getInstance(GetInstance.java:236)
            at sun.security.jca.GetInstance.getInstance(GetInstance.java:164)
            at javax.net.ssl.SSLContext.getInstance(SSLContext.java:168)
            at org.eclipse.jetty.util.ssl.SslContextFactory.getSSLContextInstance(SslContextFactory.java:1845)
            at org.eclipse.jetty.util.ssl.SslContextFactory.load(SslContextFactory.java:317)
            at org.eclipse.jetty.util.ssl.SslContextFactory.doStart(SslContextFactory.java:245)
            at org.eclipse.jetty.util.component.AbstractLifeCycle.start(AbstractLifeCycle.java:72)
            at org.eclipse.jetty.util.component.ContainerLifeCycle.start(ContainerLifeCycle.java:169)
            at org.eclipse.jetty.util.component.ContainerLifeCycle.doStart(ContainerLifeCycle.java:117)
            at org.eclipse.jetty.client.HttpClient.doStart(HttpClient.java:244)
            at org.eclipse.jetty.util.component.AbstractLifeCycle.start(AbstractLifeCycle.java:72)
            at cognitect.http_client$create.invokeStatic(http_client.clj:311)
            at cognitect.aws.http.cognitect$create.invokeStatic(cognitect.clj:5)
            at cognitect.aws.http.cognitect$create.invoke(cognitect.clj:5)
            at cognitect.aws.http$resolve_http_client.invokeStatic(http.clj:83)
            at latacora.native_cogaws$_main.invokeStatic(native_cogaws.clj:17)
            at latacora.native_cogaws$_main.doInvoke(native_cogaws.clj:17)
            at clojure.lang.RestFn.invoke(RestFn.java:397)
            at clojure.lang.AFn.applyToHelper(AFn.java:152)
            at clojure.lang.RestFn.applyTo(RestFn.java:132)
            at latacora.native_cogaws.main(Unknown Source)
    Caused by: java.lang.NoSuchMethodException: sun.security.ssl.SSLContextImpl$TLSContext.<init>()
            at java.lang.Class.getConstructor0(DynamicHub.java:3349)
            at java.lang.Class.getConstructor(DynamicHub.java:2151)
            at java.security.Provider.newInstanceUtil(Provider.java:153)
            at java.security.Provider$Service.newInstance(Provider.java:1891)
            ... 21 more

--- src/latacora/native_cogaws.clj
+++ src/latacora/native_cogaws.clj
@@ -1,13 +1,25 @@
 (ns latacora.native-cogaws
-  (:require [cognitect.aws.client.api :as aws])
+  (:require
+   [cognitect.aws.http :as http]
+   [cognitect.aws.http.cognitect :as anothe]
+   [cognitect.aws.client.api :as aws])
   (:gen-class))
 
 (set! *warn-on-reflection* true)
 
+
+;; http/HttpClient
+;; (http/client? nil)
+;; (http/)
+((let [client  ( http/resolve-http-client nil)]
+   client))
+
+(aws/default-http-client)
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World! - I'm a native thing!")
+  (aws/default-http-client)
   (let [s3 (aws/client {:api :s3})]
     (println "s3 client has been initialized")
     ))

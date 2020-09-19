3ee83d8a470b7200bfe2f4c36b87874d9d18e64e
Sergey Alekhnovich <sergey@latacora.com>
2020-09-18 17:39:37 -0700

    skeleton for s3 write debugging

--- src/latacora/native_cogaws.clj
+++ src/latacora/native_cogaws.clj
@@ -1,5 +1,6 @@
 (ns latacora.native-cogaws
   (:require
+   [cheshire.core :as json]
    [cognitect.aws.http.cognitect]
    [cognitect.aws.protocols.rest-xml]
    [cognitect.aws.client.api :as aws])
@@ -7,12 +8,28 @@
 
 (set! *warn-on-reflection* true)
 
+;; (let [s3 (aws/client {:api :s3})
+;;       bucket "latacora-webhook-logs-test"]
+
+
+;;   (aws/invoke s3 {:op :ListBuckets})
+;;   (aws/invoke s3 {:op :ListObjectsV2 :request {
+;;                                                :Bucket bucket
+;;                                                }})
+;;   )
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World! - I'm a native thing!")
-  (let [s3 (aws/client {:api :s3})]
+  (let [s3 (aws/client {:api :s3})
+        bucket "latacora-webhook-logs-test"]
     (println "s3 client has been initialized")
     (println "here are your buckets: ")
-    (println (aws/invoke s3 {:op :ListBuckets}))))
+    (println (aws/invoke s3 {:op :ListBuckets}))
+    (println "saving a file")
+    (println (aws/invoke s3 {:op :PutObject :request {:Bucket bucket
+                                                      :Key "test/out"
+                                                      :Body (.getBytes (json/generate-string "here is some json!"))}}))  (println (aws/invoke s3 {:op :ListBuckets}))
+    ))
 
